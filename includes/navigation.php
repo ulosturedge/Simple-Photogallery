b572d6584ad88a15524da8ce644cd5e4d112575a
Leonardo Martinez <leomartinez77@yahoo.com>
2017-10-03 22:25:53 -0500

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -13,12 +13,7 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
+                    
                     <li>
                         <a href="admin/index.php">Admin</a>
                     </li>
